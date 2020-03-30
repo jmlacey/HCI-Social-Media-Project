a76e5201abd6d5c0576448db2b9c017ac84ac1ab
garcia64 <52061588+voltra40@users.noreply.github.com>
2020-03-30 12:15:57 -0400

    added component that displays profile picture

---
+++ src/Component/Anthony_Components/profilepicture.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Avatar from "react-avatar";
+import pic from "../sleepicon.png";
+
+class Picture extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="avatar">
+        <Avatar img src={pic} size="200" round={true} />
+        <button className="avatarButton"> Change Picture </button>
+      </div>
+    );
+  }
+}
+
+export default Picture;

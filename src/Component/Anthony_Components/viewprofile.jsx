dfef5993e8d36dff2a93c30c14a17824d03f0ce7
garcia64 <52061588+voltra40@users.noreply.github.com>
2020-04-14 01:35:16 -0400

    organized css files and testing buttons in profile page

--- src/Component/Anthony_Components/viewprofile.jsx
+++ src/Component/Anthony_Components/viewprofile.jsx
@@ -3,14 +3,18 @@ import Forms from "./forms";
 
 class View extends Component {
   state = {};
+  testAlert() {
+    alert("Im an alert");
+  }
   render() {
     return (
       <div className="profileFormDiv">
         <Forms />
-        <button className="profileButton" style={{ float: "right" }}>
+        <button className="profileButton leftCol1" onClick={this.testAlert}>
           {" "}
-          Done{" "}
+          Edit{" "}
         </button>
+        <button className="profileButton rightCol1"> Done </button>
       </div>
     );
   }
